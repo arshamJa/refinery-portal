3cae72e888ebfb0bcccd0da12b7116747ab9e796
Arsham Jamali <arsham.jamali.1379@Gmail.com>
2025-02-27 09:46:21 +0330

    added margin top

--- resources/views/meeting/show.blade.php
+++ resources/views/meeting/show.blade.php
@@ -2,7 +2,7 @@
 <x-app-layout>
 
 {{--    <x-template>--}}
-        <nav class="flex justify-between mb-4 mt-14">
+        <nav class="flex justify-between mb-4 mt-20">
             <ol class="inline-flex items-center mb-3 space-x-1 text-xs text-neutral-500 [&_.active-breadcrumb]:text-neutral-600 [&_.active-breadcrumb]:font-medium sm:mb-0">
                 <li class="flex items-center h-full">
                     <a href="{{route('dashboard')}}"
